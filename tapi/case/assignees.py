8e18847fb8c8819d8d3d3f796823709ed17138f6
1Doomdie1 <horia.todoran@yahoo.com>
2025-01-29 17:29:55 +0000

    Changed functions to make use of the api_version param; linting

--- tapi/case/assignees.py
+++ tapi/case/assignees.py
@@ -8,12 +8,14 @@ class CaseAssigneesAPI(Client):
 
     def list(
             self,
-            case_id: int,
+            case_id:  int,
             per_page: int = 10,
-            page: int = 1,
+            page:     int = 1,
     ):
         return self._http_request(
             "GET",
             f"{self.base_endpoint}/{case_id}/assignees",
-            json = {key: value for key, value in locals().items() if value is not None and key not in ("self", "case_id")}
+            "v2",
+            json = {key: value for key, value in locals().items() if
+                    value is not None and key not in ("self", "case_id")}
         )
