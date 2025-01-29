9ae993ddd4ece7b1067675377e45bf5e0323a03f
1Doomdie1 <horia.todoran@yahoo.com>
2025-01-29 17:29:40 +0000

    added tests for CaseAssigneesAPI

---
+++ tests/test_tapi/test_case/test_assignees.py
@@ -0,0 +1,21 @@
+import unittest
+from os          import getenv
+from dotenv      import load_dotenv
+from tapi        import CaseAssigneesAPI
+
+
+class test_CaseAssigneesAPI(unittest.TestCase):
+    def setUp(self):
+        load_dotenv()
+        self.case_id            = int(getenv("CASE_ID"))
+        self.case_assignees_api = CaseAssigneesAPI(getenv("DOMAIN"), getenv("API_KEY"))
+
+    def test_list(self):
+        resp = self.case_assignees_api.list(
+            case_id = self.case_id
+        )
+
+        body = resp.get("body")
+
+        self.assertEqual(resp.get("status_code"), 200)
+        self.assertEqual(type(body.get("assignees")), list)
