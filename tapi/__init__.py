3734c4d4e95b492d8fb7a4beccc8073165435e51
1Doomdie1 <horia.todoran@yahoo.com>
2025-01-29 21:32:52 +0000

    added new imports

--- tapi/__init__.py
+++ tapi/__init__.py
@@ -5,7 +5,10 @@ from .tenant import TenantAPI
 
 __all__ = [
     "TenantAPI",
+
     "CaseAPI", "CaseActionsAPI", "CaseActivitiesAPI", "CaseAssigneesAPI", "CaseInputsAPI", "CaseInputsFieldsAPI",
+    "CaseCommentsAPI", "CaseCommentsReactionsAPI",
+
     "StoriesAPI", "ChangeRequestAPI", "RunsAPI", "VersionsAPI",
     "TeamsAPI", "MembersAPI"
 ]
--- tapi/case/__init__.py
+++ tapi/case/__init__.py
@@ -3,7 +3,9 @@ from .actions      import CaseActionsAPI
 from .assignees    import CaseAssigneesAPI
 from .activities   import CaseActivitiesAPI
 from .inputs       import CaseInputsAPI, CaseInputsFieldsAPI
+from .comments     import CaseCommentsAPI, CaseCommentsReactionsAPI
 
 __all__ = [
-    "CaseAPI", "CaseActionsAPI", "CaseAssigneesAPI", "CaseActivitiesAPI", "CaseInputsAPI", "CaseInputsFieldsAPI"
+    "CaseAPI", "CaseActionsAPI", "CaseAssigneesAPI", "CaseActivitiesAPI", "CaseInputsAPI", "CaseInputsFieldsAPI",
+    "CaseCommentsAPI", "CaseCommentsReactionsAPI"
 ]
