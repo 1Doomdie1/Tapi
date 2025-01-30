89bf57b546bb685a7a6c21862f27f4a148c14884
Todoran <horia.todoran@tesco.com>
2025-01-30 16:31:29 +0000

    linting

--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="tapi",
-    version="0.0.2",
-    description="Tines REST API wrapper",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/1Doomdie1/Tapi",
-    author="Todoran Horia",
-    license="GPL-3.0",
-    classifiers=[
+    name                          = "tapi",
+    version                       = "0.0.2",
+    description                   = "Tines REST API wrapper",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    url                           = "https://github.com/1Doomdie1/Tapi",
+    author                        = "Todoran Horia",
+    license                       = "GPL-3.0",
+    classifiers                   = [
         "License :: OSI Approved :: GPL-3.0 License",
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    install_requires=req_list,
-    extras_require={
+    install_requires              = req_list,
+    extras_require                = {
         "dev": ["pytest", "twine"],
     },
-    python_requires=">=3.10",
+    python_requires               =">=3.10"
 )
