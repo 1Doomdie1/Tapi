041c38c9c9e49c240f3d47ab02e471e32ba6ce8e
1Doomdie1 <horia.todoran@yahoo.com>
2025-02-03 18:06:12 +0000

    added tests for case records

---
+++ tests/test_tapi/test_case/test_records.py
@@ -0,0 +1,58 @@
+import unittest
+from os     import getenv
+from dotenv import load_dotenv
+from tapi   import CaseRecordsAPI
+
+
+class test_CaseFilesAPI(unittest.TestCase):
+    def setUp(self):
+        load_dotenv()
+        self.case_id          = int(getenv("CASE_ID"))
+        self.record_id        = int(getenv("CASE_RECORD_ID"))
+        self.delete_record_id = int(getenv("CASE_DELETE_RECORD_ID"))
+        self.case_records_api = CaseRecordsAPI(getenv("DOMAIN"), getenv("API_KEY"))
+
+    def test_create(self):
+        resp = self.case_records_api.create(
+            case_id   = self.case_id,
+            record_id = self.record_id
+        )
+
+        self.assertEqual(resp.get("status_code"), 201)
+
+    def test_get(self):
+        resp = self.case_records_api.get(
+            case_id   = self.case_id,
+            record_id = self.record_id
+        )
+
+        body = resp.get("body")
+
+        self.assertEqual(resp.get("status_code"), 200)
+        self.assertEqual(body.get("record").get("id"), self.record_id)
+        self.assertEqual(body.get("record").get("record_type"), {"id": 1419, "name": "Record Unit Test"})
+
+    def test_list(self):
+        resp = self.case_records_api.list(
+            case_id = self.case_id
+        )
+
+        body = resp.get("body")
+
+        self.assertEqual(resp.get("status_code"), 200)
+        self.assertEqual(type(body.get("records")), list)
+
+    def test_delete(self):
+        record = self.case_records_api.create(
+            case_id   = self.case_id,
+            record_id = self.delete_record_id
+        )
+
+        record_id = record.get("body").get("record").get("id")
+
+        resp = self.case_records_api.delete(
+            case_id   = self.case_id,
+            record_id = record_id
+        )
+
+        self.assertEqual(resp.get("status_code"), 204)
