aeba4cd4c1581ef2f2a2efafe453060b5da46e2c
1Doomdie1 <horia.todoran@yahoo.com>
2025-02-04 10:51:18 +0000

    incremented package version in setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name                          = "tapi",
-    version                       = "0.0.2",
+    version                       = "0.0.3",
     description                   = "Tines REST API wrapper",
     long_description              = long_description,
     long_description_content_type = "text/markdown",
