e5795829beb7bc1f9c0d6cb6344ac4e6cbbb1763
1Doomdie1 <horia.todoran@yahoo.com>
2025-03-10 23:13:19 +0000

    added support for self hosted tenants enpoints

--- tapi/api/tenant.py
+++ tapi/api/tenant.py
@@ -14,16 +14,32 @@ from .credential import CredentialsAPI
 class TenantAPI(Client):
     def __init__(self, domain: str, apiKey: str):
         super().__init__(domain, apiKey)
-        self.cases       = CaseAPI(domain, apiKey)
-        self.teams       = TeamsAPI(domain, apiKey)
-        self.events      = EventsAPI(domain, apiKey)
-        self.stories     = StoriesAPI(domain, apiKey)
-        self.folders     = FoldersAPI(domain, apiKey)
-        self.records     = RecordsAPI(domain, apiKey)
-        self.resources   = ResourcesAPI(domain, apiKey)
-        self.reporting   = ReportingAPI(domain, apiKey)
-        self.audit_logs  = AuditLogsAPI(domain, apiKey)
-        self.credentials = CredentialsAPI(domain, apiKey)
+        self.base_endpoint = "info"
+        self.cases         = CaseAPI(domain, apiKey)
+        self.teams         = TeamsAPI(domain, apiKey)
+        self.events        = EventsAPI(domain, apiKey)
+        self.stories       = StoriesAPI(domain, apiKey)
+        self.folders       = FoldersAPI(domain, apiKey)
+        self.records       = RecordsAPI(domain, apiKey)
+        self.resources     = ResourcesAPI(domain, apiKey)
+        self.reporting     = ReportingAPI(domain, apiKey)
+        self.audit_logs    = AuditLogsAPI(domain, apiKey)
+        self.credentials   = CredentialsAPI(domain, apiKey)
 
     def info(self):
-        return self._http_request("GET", "info")
+        return self._http_request(
+            "GET",
+            self.base_endpoint
+        )
+
+    def web_statistics(self):
+        return self._http_request(
+            "GET",
+            f"{self.base_endpoint}/web_stats"
+        )
+
+    def worker_statistics(self):
+        return self._http_request(
+            "GET",
+            f"{self.base_endpoint}/worker_stats"
+        )
